da7de9d4f935d81cf0a1bad97f7d95d136d3527e
Josh Vega <jsvcycling.live@gmail.com>
2020-04-20 21:58:11 -0400

    Selecting fields from data.

--- src/main/java/edu/njit/jsv28/cs643/project/mappers/AllHourlyStatisticsMapper.java
+++ src/main/java/edu/njit/jsv28/cs643/project/mappers/AllHourlyStatisticsMapper.java
@@ -1,9 +1,7 @@
 package edu.njit.jsv28.cs643.project.mappers;
 
 import edu.njit.jsv28.cs643.project.utils.Constants;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.MapWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
@@ -30,7 +28,13 @@ public class AllHourlyStatisticsMapper extends Mapper<LongWritable, Text, Text, 
 
         MapWritable result = new MapWritable();
 
-        // TODO: Select fields.
+        result.put(new Text("passenger_count"), new IntWritable(Integer.parseInt(parts[3])));
+        result.put(new Text("trip_distance"), new FloatWritable(Float.parseFloat(parts[4])));
+        result.put(new Text("PULocationID"), new IntWritable(Integer.parseInt(parts[7])));
+        result.put(new Text("DOLocationID"), new IntWritable(Integer.parseInt(parts[8])));
+        result.put(new Text("fare_amount"), new FloatWritable(Float.parseFloat(parts[10])));
+        result.put(new Text("tip_amount"), new FloatWritable(Float.parseFloat(parts[13])));
+        result.put(new Text("total_amount"), new FloatWritable(Float.parseFloat(parts[16])));
 
         context.write(new Text(pickupTime), result);
     }
